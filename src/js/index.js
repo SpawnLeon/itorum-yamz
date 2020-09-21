1412915d3e30c795acc9d6e1f9eb6aa477b96f8e
Alexander Leonov <spawnleon@gmail.com>
2020-09-21 17:58:58 +0300

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// import Vue from 'vue';
+// window.Vue = Vue;
+
+const jquery = require('jquery');
+const $ = require('jquery');
+const jQuery = require('jquery');
+window.jQuery = $;
+const fancybox = require('@fancyapps/fancybox');
+
+const axios = require('axios').default;
+window.axios = axios;
+
+import Swiper from 'swiper';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+});
+
+
+
+
