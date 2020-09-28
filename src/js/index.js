e6ea90ac45934795ce7716a65a4584a049b9c3ff
Alexander Leonov <spawnleon@gmail.com>
2020-09-28 16:17:34 +0300

    saving

--- src/js/index.js
+++ src/js/index.js
@@ -15,19 +15,87 @@ window.axios = axios;
 import Swiper from 'swiper';
 
 document.addEventListener('DOMContentLoaded', () => {
-  Array.from(document.querySelectorAll('.item-form__field--text')).forEach((el, i) => {
-    el.addEventListener('focus', function(event) {
-      this.nextElementSibling.classList.add('item-form__name--focus');
-    });
-    el.addEventListener('blur', function(event) {
-      if (!this.value) {
-        this.nextElementSibling.classList.remove('item-form__name--focus');
+  Array.from(document.querySelectorAll('.item-form__field--text')).
+    forEach((el, i) => {
+      el.addEventListener('focus', function(event) {
+        this.nextElementSibling.classList.add('item-form__name--focus');
+      });
+      el.addEventListener('blur', function(event) {
+        if (!this.value) {
+          this.nextElementSibling.classList.remove('item-form__name--focus');
+        }
+      });
+      if (el.value) {
+        el.nextElementSibling.classList.add('item-form__name--focus');
       }
     });
-    if (el.value) {
-      el.nextElementSibling.classList.add('item-form__name--focus');
-    }
+
+  // product slider
+  const galleryThumbs = new Swiper('.images-product-thumbs__container', {
+    spaceBetween: 10,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    direction: 'vertical',
+    slidesPerColumnFill: 'column',
   });
+  const galleryTop = new Swiper('.images-product__container', {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+  });
+
+  (function() {
+    const fixedHeader = document.querySelector('[data-js-fixed]');
+
+    if (fixedHeader) {
+      const topFixedHeader = fixedHeader.getBoundingClientRect().top +
+        document.body.scrollTop;
+
+      const setFixedHeader = function(fixedHeaderHeight = 0) {
+        const currentTopScroll = document.documentElement.scrollTop;
+
+        if (fixedHeaderHeight < currentTopScroll) {
+          document.body.classList.add('has-fixed-header');
+          document.body.style.paddingTop = fixedHeaderHeight + 'px';
+
+        } else {
+          document.body.classList.remove('has-fixed-header');
+          document.body.style.paddingTop = '0px';
+        }
+      };
+
+      const fixedHeaderHeight = fixedHeader.clientHeight;
+      window.addEventListener('scroll', function() {
+        setFixedHeader(fixedHeaderHeight);
+      });
+      setFixedHeader();
+    }
+  })();
+
+  //custom tabs
+  document.querySelectorAll('[data-tab-target]').forEach((el, i) => {
+    el.addEventListener('change', function(event) {
+      event.preventDefault();
+      document.querySelectorAll('[data-tab]').forEach((el, i) => {
+        el.hidden = true;
+      });
+
+      const target = this.dataset.tabTarget;
+      const targetTab = document.querySelector(`[data-tab="${target}"]`);
+      if (targetTab) {
+        targetTab.hidden = false;
+      }
+    });
+  });
+
+
 });
 
 
